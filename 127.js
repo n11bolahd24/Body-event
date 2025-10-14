7915c8dd33f275ee9813a43994286aca9647e836
n11bolahd <novendibagus5@gmail.com>
2025-10-15 03:57:14 +0700

    Update and rename 126.js to 127.js

--- 127.js
+++ 127.js
@@ -59,7 +59,7 @@ function renderMatch(matchId, matchKey, serverFuncs, boxClass = "kotak", tvServe
           const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
           const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
           const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-          tvCountdownEl.innerHTML = hours + "h " + minutes + "m " + seconds + "s";
+          tvCountdownEl.innerHTML = "TV Server starts in: " + hours + "h " + minutes + "m " + seconds + "s";
           // Nonaktifkan klik & bikin transparan
           tvServers.forEach(s => {
             s.style.pointerEvents = "none";
