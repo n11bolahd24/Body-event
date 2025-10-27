0d802c7ae4071b82355237ab955410802eb1b4c5
n11bolahd <novendibagus5@gmail.com>
2025-10-27 21:34:38 +0700

    Update JWPLAYER-channel.js

--- JWPLAYER-channel.js
+++ JWPLAYER-channel.js
@@ -56,7 +56,7 @@ function initChannelButton(player, streams) {
 
   // Tombol reload
   const reloadItem = document.createElement("div");
-  reloadItem.innerText = "🔄 RELOAD CHANNEL";
+  reloadItem.innerText = "🔄 RELOAD LIST CHANNEL";
   Object.assign(reloadItem.style, {
     padding: "8px 12px",
     cursor: "pointer",
