f5bbf7010578ef1177737501652fe199ee5bc75f
n11bolahd <novendibagus5@gmail.com>
2025-11-06 05:39:44 +0700

    Update and rename kotaksofa108.js to kotaksofa109.js

--- kotaksofa109.js
+++ kotaksofa109.js
@@ -53,7 +53,7 @@ function renderMatch(matchId, matchKey, serverFuncs, boxClass = "kotak", tvServe
       <!-- Tim Home -->
 <div style="flex:1; display:flex; justify-content:flex-end; align-items:center; gap:8px;">
   <span id="teamshome${matchKey}" 
-        style="font-weight:bold; color:white; font-size:14px; text-align:right; white-space:normal; word-wrap:break-word; max-width:116px; display:inline-block; line-height:1.2;">
+        style="font-weight:bold; color:white; font-size:14px; text-align:right; white-space:normal; word-wrap:break-word; max-width:112px; display:inline-block; line-height:1.2;">
   </span>
   <img id="logoHome${matchKey}" style="height:45px; width:45px; border-radius:5px; margin-right:15px;">
 </div>
@@ -66,7 +66,7 @@ function renderMatch(matchId, matchKey, serverFuncs, boxClass = "kotak", tvServe
 <div style="flex:1; display:flex; justify-content:flex-start; align-items:center; gap:8px;">
   <img id="logoAway${matchKey}" style="height:45px; width:45px; border-radius:5px; margin-left:15px;">
   <span id="teamsaway${matchKey}" 
-        style="font-weight:bold; color:white; font-size:14px; text-align:left; white-space:normal; word-wrap:break-word; max-width:116px; display:inline-block; line-height:1.2;">
+        style="font-weight:bold; color:white; font-size:14px; text-align:left; white-space:normal; word-wrap:break-word; max-width:112px; display:inline-block; line-height:1.2;">
   </span>
 </div>
 
