e06af8c4016b90f7975b6aa2029112733fd0240b
n11bolahd <novendibagus5@gmail.com>
2025-11-05 21:44:38 +0700

    Update and rename kotaksofa103.js to kotaksofa104.js

--- kotaksofa104.js
+++ kotaksofa104.js
@@ -13,7 +13,8 @@ function renderMatch(matchId, matchKey, serverFuncs, boxClass = "kotak", tvServe
     
     <div class="countdown" id="countdown${matchKey}"></div>
     <div class="live-container" id="liveContainer${matchKey}" style="text-align:center; height:20px;">
-      <span id="liveStatus${matchKey}" style="display:inline-block; width:150px; font-weight:bold;"></span>
+    <div id="liveStatus${matchKey}" 
+      style="display:inline-block; width:150px; font-weight:bold; position:relative; margin-top:-5px;"></div>
     </div>
 </div>
 
