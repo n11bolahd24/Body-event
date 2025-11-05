4873745b631624dc4d999e5e8510b75eaa87d181
n11bolahd <novendibagus5@gmail.com>
2025-11-05 17:58:44 +0700

    Update and rename kotaksofa30.js to kotaksofa31.js

--- kotaksofa31.js
+++ kotaksofa31.js
@@ -46,7 +46,7 @@ function renderMatch(matchId, matchKey, serverFuncs, boxClass = "kotak", tvServe
       width:100%;
       max-width:420px;
       margin-top:10px;
-      margin-bottom:5px;
+      margin-bottom:10px;
     ">
           
       <!-- Tim Home -->
