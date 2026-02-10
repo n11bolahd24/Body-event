fb2e10c9af9af4ae8843d68f0d27725201fb6d60
n11bolahd <novendibagus5@gmail.com>
2026-02-10 16:41:31 +0700

    Update and rename POSTINGAN1.js to POSTINGAN2.js

---
+++ POSTINGAN2.js
@@ -0,0 +1,13 @@
+<!-- GROUP BEIN -->
+<a href="javascript:bein1aus();">
+  <img src="https://cdn.jsdelivr.net/gh/habotv/sptv@master/tv26.png">
+</a>
+<a href="javascript:bein2aus();">
+  <img src="https://cdn.jsdelivr.net/gh/habotv/sptv@master/tv27.png">
+</a>
+<a href="javascript:bein3aus();">
+  <img src="https://cdn.jsdelivr.net/gh/habotv/sptv@master/tv28.png">
+</a>
+<a href="javascript:bein4();">
+  <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi47P4FOnbFpWvnvhS1k-yu7WaVJ2dCgNPjK6pIl10vYRcNbzvkXSAFFdRA3IAbdvQ7RS0eFY7OWvydVCFmtS539iYuROaOHdPkPgM4Hi6cmCafL7H9h1_xCedDu_8xwSlF6l9DuSz6AZDGnPQQL3dsBYdEySa85ED-SLJaQtU_UaVzQkKZbFkufj5HP3fP/s320/tv29.png">
+</a>
