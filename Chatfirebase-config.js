5da4ade126a9b3a921f6e2361114c8b50f825883
n11bolahd <novendibagus5@gmail.com>
2026-06-28 02:13:59 +0700

    Create Chatfirebase-config.js

---
+++ Chatfirebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBueFmieBcf4uhGsZPpfBrbQj7-cqIKGiY",
+  authDomain: "live-chat-n11bolahd.firebaseapp.com",
+  databaseURL: "https://live-chat-n11bolahd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "live-chat-n11bolahd",
+  storageBucket: "live-chat-n11bolahd.firebasestorage.app",
+  messagingSenderId: "188799799334",
+  appId: "1:188799799334:web:e6feba8bf77ae779e496e7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
