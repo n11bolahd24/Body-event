807875c465b5ae653ae53f2ee05c9730c8e99b76
n11bolahd <novendibagus5@gmail.com>
2026-07-25 16:19:30 +0700

    Update and rename Cola2.0-28.js to Cola2.0-27.js

--- Cola2.0-27.js
+++ Cola2.0-27.js
@@ -1405,9 +1405,11 @@ async function playColaStream(url){
 <div id="shakaContainer"
 class="shaka-video-container"
 style="
-position:relative;
-width:100%;
-height:100%;
+position:absolute;
+top:0;
+left:0;
+right:0;
+bottom:0;
 ">
 
 <video id="colaVideo"
